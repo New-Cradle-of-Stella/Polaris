95e8f5e6d3d34591e62909d31cb1ebb7dae27e56
AAAA9731 <alonprc@gmail.com>
2026-08-15 16:28:26 +0800

    replace modules with git submodules

--- PolarisEvent/Diagnostics/DiagnosticSeverity.cs
+++
@@ -1,9 +0,0 @@
-namespace Polaris.Pevt.Diagnostics
-{
-    /// <summary>诊断严重级。语法设计草案与静态诊断表约定：Error 阻止事件进入可执行状态，Warning 不阻止。</summary>
-    public enum DiagnosticSeverity
-    {
-        Warning,
-        Error,
-    }
-}
